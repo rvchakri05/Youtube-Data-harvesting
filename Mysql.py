9d35b61bba0e6e8a08f3807fa72ad3429fc5dc75
rvchakri05 <125747886+rvchakri05@users.noreply.github.com>
2024-03-31 20:08:02 +0530

    Mysql.py

--- Mysql.py
+++ Mysql.py
@@ -1,7 +1,9 @@
 from youtube import *
 import mysql.connector
 import youtube
-
+from sqlalchemy import create_engine
+engine=create_engine(url="mysql+mysqlconnector://{0}:{1}@{2}:{3}/{4}".format(
+            user, password, host, port, database_name))
 def is_database_exists(database_name):
     connection = mysql.connector.connect(
         host=host,
@@ -51,7 +53,7 @@ def create_tables(table_name):
                 id VARCHAR(255) PRIMARY KEY,
                 c_id VARCHAR(255),
                 v_name VARCHAR(1000),
-                v_description VARCHAR(2000),
+                v_description VARCHAR(10000),
                 V_publish datetime,
                 view_count INT,
                 like_count INT,
@@ -67,10 +69,12 @@ def create_tables(table_name):
             cursor.execute(f"""CREATE TABLE IF NOT EXISTS {table_name} (
                 id VARCHAR(255) PRIMARY KEY,
                 v_id VARCHAR(255),
-                comment_text VARCHAR(1000),
+                c_id VARCHAR(255),
+                comment_text VARCHAR(8000),
                 comment_author VARCHAR(255),
                 comment_published datetime,
-                FOREIGN KEY(v_id) REFERENCES video_data(id) ON DELETE CASCADE
+                FOREIGN KEY(v_id) REFERENCES video_data(id),
+                FOREIGN KEY(c_id) REFERENCES channel_data(id) ON DELETE CASCADE
             )""")
         else:
             print("Invalid table name")
@@ -96,17 +100,27 @@ def insert_data_into_table(table_name):
         print(f"Error: {err}")
 def id_exist(channel_id):
     connection = mysql.connector.connect(
-                    host=host,
-                    user=user,
-                    password=password,
-                    database=database_name)
+        host=host,
+        user=user,
+        password=password)
     cursor = connection.cursor()
-    cursor.execute(f"SELECT COUNT(*) FROM channel_data WHERE id= '{channel_id}'")
-    result = cursor.fetchone()[0]
-    if result > 0:
-        cursor.execute(f'DELETE FROM channel_data WHERE id={channel_id}')
-    else:
+    cursor.execute("SHOW DATABASES")
+    databases = [database[0] for database in cursor.fetchall()]
+    if database_name not in databases:
         is_database_exists(database_name)
+    else:
+        connection = mysql.connector.connect(
+                        host=host,
+                        user=user,
+                        password=password,
+                        database=database_name)
+        cursor = connection.cursor()
+        cursor.execute(f"SELECT COUNT(*) FROM channel_data WHERE id= '{channel_id}'")
+        result = cursor.fetchone()[0]
+        if result > 0:
+            cursor.execute(f"DELETE FROM channel_data WHERE id='{channel_id}'")
+        else:
+            is_database_exists(database_name)
 
 
 
